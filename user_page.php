f9dce7d7b8790c39a41dcbc1b1f4212d404249ea
mccallieskater <mccallieskater@yahoo.com>
2017-11-19 14:28:23 -0600

    I added actual addresses to the links

--- user_page.php
+++ user_page.php
@@ -19,10 +19,10 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
   <div class="w3-bar w3-light-grey w3-card w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="home_page.html" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
-    <a href="" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Create Travel Request Form</a>
-    <a href="" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">View History</a>
-    <a href="" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Profile Page</a>
-    <a href="" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Account Settings</a>
+    <a href="Travel_Request_Form.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Create Travel Request Form</a>
+    <a href="account_settings.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">View History</a>
+    <a href="profilePage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Profile Page</a>
+    <a href=" account_settings.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Account Settings</a>
     <a href="" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
   </div>
 </div>
